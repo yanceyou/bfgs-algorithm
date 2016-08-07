6d96baf4ead606f1a8750792e2f8972ee6cecd42
OuYancey <ou.yancey@gmail.com>
2016-08-08 00:02:44 +0800

    finish bfgs algorithm

--- spec/BFGSAlgorithm_spec.js
+++ spec/BFGSAlgorithm_spec.js
@@ -1,8 +1,92 @@
 var BFGSAlgorithm = require('../lib/BFGSAlgorithm.js')
 
 describe("BFGSAlgorithm", function() {
-    it("test", function() {
-        var bfgs = new BFGSAlgorithm(Math.sin)
-        console.log(bfgs.step())
+    it("cacul", function() {
+        var f = function(x) {
+            return 100 * (x[1] - x[0] * x[0]) * (x[1] - x[0] * x[0]) + (1 - x[0]) * (1 - x[0])
+        }
+
+        var df = function(x) {
+            return [
+                400 * (x[0] * x[0] * x[0] - x[0] * x[1]) - 2 * (1 - x[0]),
+                200 * (x[1] - x[0] * x[0])
+            ]
+        }
+
+        var guess = [-1, 2]
+
+        var bfgs1 = new BFGSAlgorithm(f, guess, { df: df })
+        var bfgs2 = new BFGSAlgorithm(f, guess)
+
+        var result1 = bfgs1.run()
+        var result2 = bfgs2.run()
+
+        // ATTENTIONS: I change the source of jasmine-core, add this method
+        // jasmine-core version: 2.4.1
+        // L87:
+        //  'toAlmostEqual',
+        // 
+        // L2878: 
+        // getJasmineRequireObj().toAlmostEqual = function() {
+        //   function toAlmostEqual() {
+        //     return {
+        //       compare: function(actual, expected) {
+        //         return {
+        //           pass: Math.abs(actual - expected) / Math.abs(expected) < 1E-6
+        //         };
+        //       }
+        //     };
+        //   }
+        //   return toAlmostEqual;
+        // };
+        expect(result1.x[0]).toAlmostEqual(1)
+        expect(result1.x[1]).toAlmostEqual(1)
+        expect(result2.x[0]).toAlmostEqual(1)
+        expect(result2.x[1]).toAlmostEqual(1)
+    })
+
+
+    it("cacul", function() {
+        var f = function(x) {
+            return 3 * x[0] * x[0] + 2 * x[0] * x[1] + x[1] * x[1] - 4 * x[0] + 5 * x[1]      
+        }
+
+        var df = function(x) {
+            return [
+                6 * x[0] + 2 * x[1] - 4,
+                2 * x[0] + 2 * x[1] + 5
+            ]
+        }
+
+        var guess = [5, 10]
+
+        var bfgs1 = new BFGSAlgorithm(f, guess, { df: df })
+        var bfgs2 = new BFGSAlgorithm(f, guess)
+
+        var result1 = bfgs1.run()
+        var result2 = bfgs2.run()
+
+        // ATTENTIONS: I change the source of jasmine-core, add this method
+        // jasmine-core version: 2.4.1
+        // L87:
+        //  'toAlmostEqual',
+        // 
+        // L2878: 
+        // getJasmineRequireObj().toAlmostEqual = function() {
+        //   function toAlmostEqual() {
+        //     return {
+        //       compare: function(actual, expected) {
+        //         return {
+        //           pass: Math.abs(actual - expected) / Math.abs(expected) < 1E-6
+        //         };
+        //       }
+        //     };
+        //   }
+        //   return toAlmostEqual;
+        // };
+        expect(result1.x[0]).toAlmostEqual(2.25)
+        expect(result1.x[1]).toAlmostEqual(-4.75)
+        expect(result2.x[0]).toAlmostEqual(2.25)
+        expect(result2.x[1]).toAlmostEqual(-4.75)
     })
 })
--- spec/MathUtils_spec.js
+++ spec/MathUtils_spec.js
@@ -2,15 +2,50 @@ var MathUtils = require('../lib/MathUtils.js')
 var BFGSAlgorithm = require('../lib/BFGSAlgorithm.js')
 
 describe("MathUtils", function() {
+    it("have getApproximateGradientFunction method", function() {
+        var f = function(x) {
+            return 100 * (x[1] - x[0] * x[0]) * (x[1] - x[0] * x[0]) + (1 - x[0]) * (1 - x[0])
+        }
+
+        var df = function(x) {
+            return [
+                400 * (x[0] * x[0] * x[0] - x[0] * x[1]) - 2 * (1 - x[0]),
+                200 * (x[1] - x[0] * x[0])
+            ]
+        }
+
+        var x = [-1, 2]
+
+        var dff = MathUtils.getApproximateGradientFunction(f)
+
+        var res = dff(x)
+        var REAL_RESULT = df(x)
+
+        // ATTENTIONS: I change the source of jasmine-core, add this method
+        // jasmine-core version: 2.4.1
+        // L87:
+        //  'toAlmostEqual',
+        // 
+        // L2878: 
+        // getJasmineRequireObj().toAlmostEqual = function() {
+        //   function toAlmostEqual() {
+        //     return {
+        //       compare: function(actual, expected) {
+        //         return {
+        //           pass: Math.abs(actual - expected) / Math.abs(expected) < 1E-6
+        //         };
+        //       }
+        //     };
+        //   }
+        //   return toAlmostEqual;
+        // };
+        expect(res[0]).toAlmostEqual(REAL_RESULT[0])
+        expect(res[1]).toAlmostEqual(REAL_RESULT[1])
+    })
+
     it("have getIdentityMatrix method that can generate identity matrix", function() {
-        console.log(MathUtils.getIdentityMatrix)
         var matrix = MathUtils.getIdentityMatrix(3)
 
-        expect(matrix[0][0]).toBe(1)
-        expect(matrix[0][1]).toBe(0)
-        expect(matrix[1][1]).toBe(1)
-        expect(matrix[1][2]).toBe(0)
-        expect(matrix[2][2]).toBe(1)
-        expect(matrix[2][3]).toBe(undefined)
+        expect(matrix).toEqual([[1,0,0],[0,1,0],[0,0,1]])
     })
 })
